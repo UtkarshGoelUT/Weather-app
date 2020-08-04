7479e69196ea718f6554fc6d5c988bdae5f7da64
Utkarsh Goel <ugoel911@gmail.com>
2020-08-05 00:16:43 +0530

    Weather app done

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+
+const form = document.querySelector('form');
+
+const search = document.querySelector('input');
+
+const [error, weather] = document.querySelectorAll('p');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        error.textContent = "Loading...";
+        weather.textContent = "";
+
+        response.json().then((data) => {
+            if (data.error) {
+                error.textContent = data.error;
+                weather.textContent = "";
+            } else {
+                weather.textContent = data.forecast;
+                error.textContent = data.location;
+            }
+        });
+    });
+});
+
+
---
+++ src/app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const geoCode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const partialPath = path.join(__dirname, '../views/partials');
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialPath);
+
+//Serving static files
+app.use(express.static(publicPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather",
+        name: "Utkarsh"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About me",
+        name: "Utkarsh"
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: "This is help text",
+        title: "Help",
+        name: "Utkarsh"
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Address query is required'
+        });
+    }
+    const address = req.query.address;
+    geoCode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error
+            });
+        }
+        forecast(latitude, longitude, (error, data) => {
+            if (error) {
+                return res.send({
+                    error
+                });
+            }
+            res.send({
+                forecast: data,
+                location,
+                address
+            });
+        });
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'There is some error'
+        });
+    }
+    console.log(req.query);
+    res.send({
+        products: []
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404page', {
+        title: "404",
+        error: "Page not found",
+        name: 'Utkarsh'
+    });
+});
+
+
+app.get('*', (req, res) => {
+    res.render('404page', {
+        title: "404",
+        error: "Page not found",
+        name: 'Utkarsh'
+    });
+});
+
+app.listen(3000, () => { console.log('Server is up on port 3000'); })
